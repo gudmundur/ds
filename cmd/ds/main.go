eb670cea028a9bedb6c381c94e4f9f2a08eae0ed
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2014-08-19 09:44:33 +0200

    Add subcommands and cli args

--- cmd/ds/main.go
+++ cmd/ds/main.go
@@ -3,11 +3,41 @@ package main
 import (
 	"os"
 
+	"github.com/codegangsta/cli"
+
 	"github.com/gudmundur/ds"
 )
 
 func main() {
-	token := os.Getenv("DROPBOX_TOKEN")
-	ds := ds.NewDropboxSync(token)
-	ds.Sync()
+	app := cli.NewApp()
+	app.Name = "ds"
+	app.Usage = "syncs dropbox folders"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "token",
+			EnvVar: "DROPBOX_TOKEN",
+			Usage:  "Dropbox OAuth2 token",
+		},
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:  "sync",
+			Usage: "Syncs from Dropbox",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "directory",
+					Value: "tmp",
+					Usage: "Destination directory",
+				},
+			},
+			Action: func(c *cli.Context) {
+				token := c.GlobalString("token")
+				ds := ds.NewDropboxSync(token)
+				ds.Directory = c.String("directory")
+				ds.Sync()
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
